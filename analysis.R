8d4dd80d1de00a85f5fecc0511ed9533fe2807ae
ajing <ajingnk@gmail.com>
2015-05-21 15:39:10 -0400

    non-linear model

--- analysis.R
+++ analysis.R
@@ -7,9 +7,13 @@ qsar_no_na <- qsar[, colSums(!is.na(qsar)) == nrow(qsar)]
 #read the tab file using the read table function.
 
 qsar.scale<- scale(qsar_no_na,center=TRUE,scale=TRUE);
-qsar.scale_no_na <- qsar.scale[, colSums(is.na(qsar.scale)) != nrow(qsar.scale)]
+nzv <- nearZeroVar(qsar.scale)
+
+qsar.scale_no_na <- qsar.scale[, colSums(!is.na(qsar)) == nrow(qsar)]
 qsar.scale_no_na <- qsar.scale_no_na[,-c(1:4)]
-#scale all the features (from feature 2 bacause feature 1 is the predictor output)
+# remove near zero variance variable also
+nzv <- nearZeroVar(qsar.scale_no_na)
+qsar.scale_no_na <- qsar.scale_no_na[,-nzv]
 
 cor_qsar <- cor(qsar.scale_no_na, use = "complete.obs")
 #compute the correlation matrix
@@ -80,3 +84,65 @@ plot(important, las=2)
 
 lm_model_int <- lm(Ratio1 ~ density:a_nF + density:vsurf_DW13 + density:vsurf_ID8 + density:CASA..1 + density:ASA_P +  density:vsurf_HB3 + density:vsurf_HB8 + density:vsurf_HB7, data = data.frame(qsar))
 summary(lm_model_int)
+
+
+
+################# new data ####################
+qsar <- read.table("QSARalldata.csv", header = TRUE, sep = ",")
+interested <- names(important$finalDecision[important$finalDecision != "Rejected"])
+ln_model <- lm(Ratio1 ~. , data = subset(qsar, select = colnames(qsar) %in% c(interested, "Ratio1")))
+summary(ln_model)
+
+ln_model <- lm(Ratio1 ~. , data = data.frame(subset(qsar.scale, select = colnames(qsar.scale) %in% c("a_nH","BCUT_PEOE_0", "vsurf_CW6", "vsurf_HL2", "Ratio1"))))
+summary(ln_model)
+
+rfFit1 <- train(x = subset(qsar.scale, select = colnames(qsar.scale) %in% c("a_nH","BCUT_PEOE_0", "vsurf_CW6", "vsurf_HL2")), y = qsar.scale[, "Ratio1"],
+                 method = "rf",
+                 preProcess = c("center", "scale"),
+                 tuneLength = 10,
+                 trControl = trainControl(method = "LOOCV", repeats = 5))
+
+
+gbmFit1 <- train(x = subset(qsar, select = colnames(qsar) %in% c("a_nH","BCUT_PEOE_0", "vsurf_CW6", "vsurf_HL2")), y = qsar[, "Ratio1"],
+                method = "gbm",
+                preProcess = c("center", "scale"),
+                tuneLength = 10,
+                trControl = trainControl(method = "LOOCV", repeats = 5))
+
+# xgboost
+grid <- expand.grid(nrounds = seq(1, 201, by = 25),
+                    max_depth = 1:6,
+                    eta = (1:4)/10)
+xgbFit1 <- train(Ratio1 ~ ., data = subset(qsar.scale[,-nzv], select = -Ratio6),
+                 method = modelInfo,
+                 tuneGrid = grid,
+                 trControl = trainControl(method = 'LOOCV'))
+
+> max(xgbFit1$results$Rsquared)
+[1] 0.1460117
+
+xgbFit2 <- train(log(Ratio1) ~ ., data = subset(qsar, select = c(vsurf_DW13, vsurf_ID8, CASA..1, ASA_P, vsurf_HB3, density, vsurf_HB8, vsurf_HB7, Ratio1)),
+                 method = modelInfo,
+                 tuneGrid = grid,
+                 trControl = trainControl(method = 'LOOCV'))
+
+
+#xgbFinal <- xgboost(param=xgbFit1$finalModel$tuneValue, data = subset(qsar.scale[,-nzv], select = -c(Ratio1, Ratio6)), label = qsar$Ratio1, nrounds = xgbFit1$finalModel$tuneValue$nrounds)
+
+
+ctrl <- gafsControl(functions = caretGA)
+obj <- gafs(x = subset(qsar.scale, select = colnames(qsar.scale) %in% c("a_nH","BCUT_PEOE_0", "vsurf_CW6", "vsurf_HL2", "Ratio1")),
+            y = qsar.scale[, "Ratio1"],
+            iters = 100,
+            gafsControl = ctrl,
+            ## Now pass options to `train`
+            method = "")
+
+
+ctrl <- safsControl(functions = caretSA)
+obj <- safs(x = subset(qsar.scale, select = -c(Ratio1, Ratio6)),
+            y = qsar.scale[, "Ratio1"],
+            iters = 100,
+            safsControl = ctrl,
+            ## Now pass options to `train`
+            method = "gbm")
